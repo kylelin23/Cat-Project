116daff885a4a088d8f4171a283b4b1e8b1eca0a
Kyle Lin <linkyle0924@gmail.com>
2024-08-01 17:17:12 -0700

    Add starting files

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+import { View, Text } from 'react-native';
+
+export default function ExplorePage() {
+  return (
+    // Explore Page: 
+    <View>
+      <Text>Testing</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
